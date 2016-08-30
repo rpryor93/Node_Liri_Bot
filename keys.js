31b6e60405f0d80e9aab00fd4ae93aad8a76dde2
ronnie pryor <rpryor93@gmail.com>
2016-08-30 15:16:03 -0400

    added twitter api key and access token to key.js

--- keys.js
+++ keys.js
@@ -1,8 +1,8 @@
 console.log('this is loaded');
 
 exports.twitterKeys = {
-  consumer_key: '<input here>',
-  consumer_secret: '<input here>',
-  access_token_key: '<input here>',
-  access_token_secret: '<input here>',
+  consumer_key: '	71D8wU495NcEwwVi9whT9ZSfb',
+  consumer_secret: 'WEwQqm9YAx9irJxtpDx3Xdn3uzDHC2yoxclZuvgzLox8wZP0Eg',
+  access_token_key: '37946221-gsfoU2N7OAHv4EfvytpXVk13MMzV9uq0nJ0NnO0HJ',
+  access_token_secret: 'I16FcsJSqjm5oOUaEmAIH8ul4Hhs6jZIsmkl3G4c3WaUD',
 }
