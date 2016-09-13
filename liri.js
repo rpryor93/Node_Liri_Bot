d8ebfc8d56731d6c59baa5bb16deb502dcb3a630
Ronnie Pryor <rpryor93@gmail.com>
2016-09-13 14:14:06 -0400

    twitter code

--- liri.js
+++ liri.js
@@ -2,11 +2,40 @@ var keys = require('./keys.js');
 
 var Twitter = require('twitter');
 
-var client = new Twitter(keys.twitterKeys);
+var getMyTweets = function() {
 
-var params = {screen_name: 'nodejs'};
-client.get('statuses/user_timeline', params, function(error, tweets, response) {
-    if (!error) {
-        console.log(tweets);
+
+    var client = new Twitter(keys.twitterKeys);
+
+    var params = {screen_name: 'rpryor93'};
+    client.get('statuses/user_timeline', params, function(error, tweets, response) {
+        if (!error) {
+            for(var i = 0; i<tweets.length; i++) {
+                console.log(tweets[i].created_at);
+                console.log('');
+                console.log(tweets[i].text);
+
+            }
+        }
+    });
+
+}
+
+
+var pick = function(caseData, functionData) {
+    switch(caseData) {
+        case 'my-tweets':
+            getMyTweets();
+            break;
+        default:
+            console.log('liri does not know that');
     }
-});
+
+var runThis = function(argOne, argTwo) {
+    pick(argOne, argTwo);
+
+};
+
+runThis(process.argv[2], process.argv[3]);
+
+}
